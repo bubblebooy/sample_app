2bd760cd9b10dca6b07dd832269f469750cfba58
Max Garber <bubblebooy@gmail.com>
2018-08-31 17:42:47 -0500

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     puts params
     @user = User.new(whitelisted_user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new
     end
@@ -39,7 +40,7 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.paginate(page: params[:page], per_page: 30)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 30)
   end
 
   def destroy
