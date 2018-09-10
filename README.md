d5dcbc775d1b3d9154f244bc4dd04398712cae93
Max Garber <bubblebooy@gmail.com>
2018-09-10 15:19:47 -0500

    Add user microposts

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ If the test suite passes, you'll be ready to run the app in a local server:
 $ rails server
 ```
 
+Automated tests with Guard
 ```
 $ bundle exec guard
 ```
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-   before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: :destroy
 
   def new
     @user = User.new
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -58,14 +59,14 @@ class UsersController < ApplicationController
 
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  # # Confirms a logged-in user.
+  # def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger] = "Please log in."
+  #     redirect_to login_url
+  #   end
+  # end
 
   # Confirms the correct user
   def correct_user
